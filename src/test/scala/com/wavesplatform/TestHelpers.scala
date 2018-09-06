3a9b60a36bc94ac4c755c2e6339a3a5723e779a8
Bram N <bramnagtegals51@gmail.com>
2018-09-06 20:29:04 +0200

    Remove unnecesary parts

--- src/test/scala/com/wavesplatform/TestHelpers.scala
+++ src/test/scala/com/wavesplatform/TestHelpers.scala
@@ -3,10 +3,8 @@ package com.wavesplatform
 import java.io.IOException
 import java.nio.file.attribute.BasicFileAttributes
 import java.nio.file.{FileVisitResult, Files, Path, SimpleFileVisitor}
-
 import com.wavesplatform.settings.{GenesisSettings, GenesisTransactionSettings}
 import com.wavesplatform.account.Address
-
 import scala.concurrent.duration._
 
 object TestHelpers {
