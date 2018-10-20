16d6157d731be0b0489e60ffe0ca45ceab8b27d1
BlackTurtle123 <support@blackturtle.eu>
2018-10-20 11:07:50 +0200

    fix start-waves.sh

--- it/src/container/start-waves.sh
+++ it/src/container/start-waves.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-echo Options: $Agate_OPTS
-java $Agate_OPTS -jar /opt/Agate/Agate.jar /opt/Agate/template.conf
-=======
 trap 'kill -TERM $PID' TERM INT
 echo Options: $WAVES_OPTS
-java $WAVES_OPTS -jar /opt/waves/waves.jar /opt/waves/template.conf &
+java $Agate_OPTS -jar /opt/Agate/Agate.jar /opt/Agate/template.conf &
 PID=$!
 wait $PID
 trap - TERM INT
 wait $PID
 EXIT_STATUS=$?
->>>>>>> 4f3106f04982d02459cdc4705ed835b976d02dd9
